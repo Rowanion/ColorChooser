afdedcb3fa487819c52ed7f48f00401be3a2bcc2
TheAvatar <weltmarktfuehrer@googlemail.com>
2015-12-17 00:06:49 +0100

    this stuff never ends ...

--- src/ui/ColorChooser.cpp
+++ src/ui/ColorChooser.cpp
@@ -4,6 +4,12 @@ ColorChooser::ColorChooser(QWidget* parent)
 : QMainWindow(parent)
 {
 	setupUi(this);
+
+	QStringList names( QColor::colorNames() );
+	foreach( const QString& name, names)
+	{
+		
+	}
 }
 
 ColorChooser::~ColorChooser()
