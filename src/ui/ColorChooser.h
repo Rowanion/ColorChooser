242ced5948ee6a133acd2439116d91c6b943eb4d
Timo Wiesemann <timo.wiesemann@protonmail.com>
2020-11-17 23:48:22 +0000

    [Added] copy as and paste to UE4 tinting format

--- src/ui/ColorChooser.h
+++ src/ui/ColorChooser.h
@@ -6,41 +6,45 @@
 
 class ColorChooser : public QMainWindow, public Ui_ColorChooser
 {
-	Q_OBJECT
-	
+   Q_OBJECT
+
 public:
-	ColorChooser(QWidget* parent = nullptr);
-	~ColorChooser();
+   ColorChooser(QWidget* parent = nullptr);
+   ~ColorChooser();
 
 private slots:
-	void onCustomContextMenu( const QPoint & point );
-	void copyName();
-	void copyHexRGBColor();
-	void copyHexARGBColor();
-	void copyByteRGBColor();
-	void copyFloatRGBColor();
-	void onTableClicked( const QModelIndex& pIndex );
-	void on_actionCopy_As_Hex_RGB_triggered();
-	void on_actionCopy_As_Hex_ARGB_triggered();
-	void on_actionCopy_As_Byte_RGB_triggered();
-	void on_actionCopy_As_Float_RGB_triggered();
-	void on_actionPaste_From_Float_RGB_triggered();
-	void on_actionPaste_From_Byte_RGB_triggered();
-	void onUpdateClipboardContents();
-	void on_actionByteToFloat_triggered();
-	void on_actionFloatToByte_triggered();
-	void on_actionByteToHex_triggered();
-	void on_actionHexToByte_triggered();
-	void on_actionFloatToHex_triggered();
-	void on_actionHexToFloat_triggered();
+   void onCustomContextMenu(const QPoint& point);
+   void copyName();
+   void copyHexRGBColor();
+   void copyHexARGBColor();
+   void copyByteRGBColor();
+   void copyFloatRGBColor();
+   void onTableClicked(const QModelIndex& pIndex);
+   void on_actionCopy_As_Hex_RGB_triggered();
+   void on_actionCopy_As_Hex_ARGB_triggered();
+   void on_actionCopy_As_Byte_RGB_triggered();
+   void on_actionCopy_As_Float_RGB_triggered();
+   void on_actionCopy_As_UE4_Float_triggered();
+   void on_actionPaste_From_Float_RGB_triggered();
+   void on_actionPaste_From_Byte_RGB_triggered();
+   void on_actionPaste_From_UE4_Float_triggered();
+   void onUpdateClipboardContents();
+   void on_actionByteToFloat_triggered();
+   void on_actionFloatToByte_triggered();
+   void on_actionByteToHex_triggered();
+   void on_actionHexToByte_triggered();
+   void on_actionFloatToHex_triggered();
+   void on_actionHexToFloat_triggered();
 
 private:
-	class QStandardItemModel* m_model{ nullptr };
-	QString m_currentColor;
-	QTimer m_clipboardTimer;
-	class QLabel* m_clipboardContent = nullptr;
-	QColor floatStringToColor( const QString& pString );
-	QColor byteStringToColor( const QString& pString );
-	const QString colorToFloatString( const QColor& pColor );
-	const QString colorToByteString( const QColor& pColor );
+   class QStandardItemModel* m_model{ nullptr };
+   QString m_currentColor;
+   QTimer m_clipboardTimer;
+   class QLabel* m_clipboardContent = nullptr;
+   QColor floatStringToColor(const QString& pString);
+   QColor ue4StringToColor(const QString& pString);
+   QColor byteStringToColor(const QString& pString);
+   const QString colorToFloatString(const QColor& pColor);
+   const QString colorToByteString(const QColor& pColor);
+   const QString colorToUE4FloatString(const QColor& pColor);
 };
